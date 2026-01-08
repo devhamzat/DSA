bda9e83a38fee980d93affd2a3dd19d2d33e0773
devhamzat <ayomidelegbede@gmail.com>
2026-01-08 23:44:36 +0100

    solving water bottles problem

---
+++ src/leetcode/WaterBottles.java
@@ -0,0 +1,24 @@
+package leetcode;
+/*
+Thought process
+first things first, we have two integers 
+that determine the amount of water bottle you can drink.numBottles and numExchange
+so basically if numBottles = 10, and numExchange = 2,
+this means that you can exchange 2 empty bottles for one full bottle
+so;first 10= 5 exchangable , after second 5 =2 exchangeable, after third 2 =1 exchangeable
+after fourth 1 (bring back the one empty from the 2 exchangeable)so 2 = 1 exchangeable
+therefore  10+5+2+2+1=20 possible number of water you can drink
+BRUTE FORCE SOLUTION
+have a variable for the amount of bottles
+initialize it with the numbottles
+then have a variable for empty bottles
+initalize it with the
+* */
+
+public class WaterBottles {
+    public int numWaterBottles(int numBottles, int numExchange) {
+
+        return 0;
+
+    }
+}
